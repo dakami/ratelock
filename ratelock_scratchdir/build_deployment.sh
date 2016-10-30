6d4a4ed3bffcac54898368704a25a7f7c111cbd3
Your Name <you@example.com>
2016-10-30 13:38:33 +0000

    silly

--- ratelock_scratchdir/build_deployment.sh
+++ ratelock_scratchdir/build_deployment.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 apt-get -y install virtualenv python-pip
 
-ORIG=`pwd`/../
+ORIG=`pwd`
 rm -rf build
 virtualenv build
 cd build
 source ./bin/activate
 pip install dynamo3 dql
 cd lib/python2.7/site-packages/
-cp $ORIG/ratelock.py .
+cp $ORIG/../ratelock.py .
 zip -r $ORIG/ratelock.zip .
